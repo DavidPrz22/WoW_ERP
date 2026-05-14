ff979859b29f5d588b2f5afc2c800e65e8027c5a
David <deivid.prz3412@gmail.com>
2026-05-14 17:50:37 -0400

    multiple auction house support for data fetching added

--- backend/djangobackend/Registros/management/commands/get_pricing_data.py
+++ backend/djangobackend/Registros/management/commands/get_pricing_data.py
@@ -7,7 +7,6 @@ class Command(BaseCommand):
 
     OAUTH_URL_ENDPOINT = 'https://auth.tradeskillmaster.com/oauth2/token'
     PRICING_URL_ENDPOINT = 'https://pricing-api.tradeskillmaster.com'
-    AUCTION_HOUSE_NS_ALLY = 555
     
     CLIENT_ID = "c260f00d-1071-409a-992f-dda2e5498536"
     CLIENT_TOKEN = "KExtHgEPTRDJJYMJQAjyHV_xNXmopBGN" 
@@ -22,14 +21,60 @@ class Command(BaseCommand):
         response.raise_for_status()
         return response.json().get('access_token')
 
-    def pricing_data(self, token):
-        ah = AuctionHouse.objects.get(realm_name="Nightslayer", faction="Alliance")
-        url = f"{self.PRICING_URL_ENDPOINT}/ah/{ah.realm_id}"
-        response = requests.get(url, headers={'Authorization': f'Bearer {token}'})
-        response.raise_for_status()
-        return response.json()
+    def pricing_data(self, token, **realms):
+        ah = AuctionHouse.objects.filter(realm_name__in=realms.values())
+        pricing_data_ah = {}
+        for action_house in ah:
+            url = f"{self.PRICING_URL_ENDPOINT}/ah/{action_house.realm_id}"
+            response = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+            response.raise_for_status()
+            
+            key = f"{action_house.realm_name}-{action_house.faction}"
+            pricing_data_ah[key] = response.json()
+            
+        return pricing_data_ah
+
+    def save_pricing_data(self, record_data: dict):
+        existing_items = {item.id_ingame: item for item in Item.objects.all()}
+
+        for key, data in record_data.items():
+            realm, faction = key.split('-')
+            
+            try:
+                auction_house = AuctionHouse.objects.get(realm_name=realm, faction=faction)
+            except AuctionHouse.DoesNotExist:
+                self.stderr.write(f"AuctionHouse not found for {realm}-{faction}")
+                continue
+
+            record = Records.objects.create(auction_house=auction_house)
+            items_records = []
+            
+            for item_data in data:
+                item_id_str = str(item_data.get('itemId'))
+                
+                if item_id_str in existing_items:
+                    item_obj = existing_items[item_id_str]
+                    
+                    items_records.append(
+                        ItemRecord(
+                            item=item_obj,
+                            record=record,
+                            market_value=item_data.get('marketValue', 0),
+                            min_buyout=item_data.get('minBuyout', 0),
+                            num_auctions=item_data.get('numAuctions', 0),
+                            historical=item_data.get('historical', 0)
+                        )
+                    )
+
+            if items_records:
+                ItemRecord.objects.bulk_create(items_records, batch_size=1000)
+            
+            self.stdout.write(self.style.SUCCESS(f'Successfully updated pricing for {len(items_records)} items in {key}.'))
 
     def handle(self, *args, **options):
+        REALMS = {
+            'Nightslayer': 'Nightslayer'
+        }
         self.stdout.write("Fetching access token...")
         try:
             token = self.get_access_token()
@@ -39,41 +84,12 @@ class Command(BaseCommand):
 
         self.stdout.write("Fetching pricing data...")
         try:
-            data = self.pricing_data(token)
+            data: dict = self.pricing_data(token, **REALMS)
         except requests.exceptions.RequestException as e:
             self.stderr.write(f"Failed to get pricing data: {e}")
             return
-
+        
         self.stdout.write("Saving data to database...")
-        
-
-        existing_items = {item.id_ingame: item for item in Item.objects.all()}
-        
-        items_records = []
-
-        ah = AuctionHouse.objects.get(realm_name="Nightslayer", faction="Alliance")
-
-        record = Records.objects.create(auction_house = ah)
-        
-        for item_data in data:
-            item_id_str = str(item_data.get('itemId'))
-            
-            if item_id_str in existing_items:
-                item_obj = existing_items[item_id_str]
-                
-                items_records.append(
-                    ItemRecord(
-                        item=item_obj,
-                        record=record,
-                        market_value=item_data.get('marketValue', 0),
-                        min_buyout=item_data.get('minBuyout', 0),
-                        num_auctions=item_data.get('numAuctions', 0),
-                        historical=item_data.get('historical', 0)
-                    )
-                )
-
-        if items_records:
-            ItemRecord.objects.bulk_create(items_records, batch_size=1000)
-            
-        self.stdout.write(self.style.SUCCESS(f'Successfully updated pricing for {len(items_records)} items.'))
+        self.save_pricing_data(data)
+g
 
